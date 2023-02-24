cec6ee4b0d7e24110e76b66466cc7beffa46564c
Island Chen <flyisland@gmail.com>
2023-02-24 06:38:50 +0000

    fix: check labels

--- src/index.jsx
+++ src/index.jsx
@@ -4,37 +4,32 @@ export async function run(event, context) {
   if (!event) {
     console.log("Invalid event");
   } else {
-    const contentType=event["content"]["type"]
-    if (contentType!=="page"){
+    const contentType = event["content"]["type"]
+    if (contentType !== "page") {
       console.log(`Invalid content type '${contentType}'`, JSON.stringify(event, null, 2));
-    }else{
-      const contentId=event["content"]["id"]
-      await checkPageProperties(contentId)
-      await checkPageLabels(contentId)
+    } else {
+      const pageId = event["content"]["id"]
+      console.log(`Page "${pageId}/${event["content"]["title"]}" is ${event["eventType"].split(":")[2]}`)
+      await convertSummaryToLabels(pageId)
     }
   }
 }
 
-async function checkPageProperties(pageId){
-  const propUrl = route`/wiki/api/v2/pages/${pageId}/properties` 
-  console.log(propUrl)
+async function convertSummaryToLabels(pageId) {
+  // 1. fetch labels and page body
+  const propUrl = route`/wiki/rest/api/content/${pageId}?expand=metadata.labels,body.storage`
   const response = await api.asApp().requestConfluence(propUrl, {
-    headers: {
-      'Accept': 'application/json'
-    }
-  });  
-  console.log(`Response: ${response.status} ${response.statusText}`);
-  console.log(await response.json());
-}
-
-async function checkPageLabels(pageId){
-  const labelsUrl = route`/wiki/api/v2/pages/${pageId}/labels` 
-  console.log(labelsUrl)
-  const response = await api.asApp().requestConfluence(labelsUrl, {
-    headers: {
-      'Accept': 'application/json'
-    }
-  });  
-  console.log(`Response: ${response.status} ${response.statusText}`);
-  console.log(await response.json());
+    headers: { 'Accept': 'application/json' }
+  });
+  if (response.status !== 200) {
+    console.error(`Response: ${response.status} ${response.statusText}`);
+    console.error(await response.text());
+    return;
+  }
+  const responseJson = await response.json()
+  const labels = responseJson["metadata"]["labels"]["results"].map(item => item["label"])
+  if (!labels.includes('se-tsd') && !labels.includes('se-opportunity')) {
+    console.error("This is not a valid TSD page!")
+    return
+  }
 }
