e9eb16191abb7092a9b91f8416aad23d9d5c1b24
Island Chen <flyisland@gmail.com>
2023-03-06 14:10:26 +0000

    feat: support --all

--- src/labelcli.mjs
+++ src/labelcli.mjs
@@ -15,13 +15,37 @@ export class LabelOperator {
   constructor(options) {
     this.options = options
 
-    this.baseUrl = `https://${options.domain}.atlassian.net`
+    this.baseUrl = `https://${options.domain}.atlassian.net/wiki`
     this.Authorization = `Basic ${Buffer.from(`${options.forgeEmail}:${options.forgeApiToken}`).toString('base64')}`
     this.agent = new https.Agent({ keepAlive: true })
   }
 
   run() {
-    this.updateLabelsForPage(this.options.pageId)
+    if (this.options.pageId) {
+      this.updateLabelsForPage(this.options.pageId)
+    } else if (this.options.all) {
+      this.updateLabelsForAllPages()
+    }
+  }
+
+  updateLabelsForAllPages() {
+    const cql = 'space=AT and type=page and (label="se-opportunity" or label="se-tsd")'
+    let path = encodeURI(`/rest/api/content/search?cql=${cql}&limit=50`)
+    this.fetchAllPages(path)
+      .then(results => results.forEach(page => console.info(`${page.id}: ${page.title}`)))
+  }
+
+  fetchAllPages(path, results = []) {
+    return this.requestConfluence("GET", path, 200)
+      .then(response => response.json())
+      .then(responseJson => {
+        const newResults = results.concat(responseJson.results)
+        if (responseJson._links.next) {
+          return this.fetchAllPages(responseJson._links.next, newResults)
+        } else {
+          return newResults
+        }
+      })
   }
 
   updateLabelsForPage(pageId) {
@@ -33,10 +57,6 @@ export class LabelOperator {
       })
       .then(labels => Promise.all([this.addLabels(pageId, labels.labelsToAdd), this.removeLabels(pageId, labels.labelsToRemove)]))
       .catch(err => console.error(err))
-  }
-
-  handleAllPages() {
-
   }
 
   async requestConfluence(method, path, expectedCode, body = {}) {
@@ -66,7 +86,7 @@ export class LabelOperator {
   }
 
   fetchPageContent(pageId) {
-    const path = `/wiki/rest/api/content/${pageId}?expand=metadata.labels,body.storage`
+    const path = `/rest/api/content/${pageId}?expand=metadata.labels,body.storage`
     return this.requestConfluence("GET", path, 200)
   }
 
@@ -74,47 +94,15 @@ export class LabelOperator {
     console.info("labelsToAdd:", JSON.stringify(labelsToAdd))
     if (labelsToAdd.length === 0) { return }
     const bodyData = toLabelsBody(labelsToAdd)
-    const path = `/wiki/rest/api/content/${pageId}/label`
+    const path = `/rest/api/content/${pageId}/label`
     return this.requestConfluence("POST", path, 200, { body: JSON.stringify(bodyData) })
   }
 
   removeLabels(pageId, labelsToRemove) {
     console.info("labelsToRemove:", JSON.stringify(labelsToRemove))
     return Promise.all(labelsToRemove.map(label => {
-      const path = `/wiki/rest/api/content/${pageId}/label/${label}`
+      const path = `/rest/api/content/${pageId}/label/${label}`
       return this.requestConfluence("DELETE", path, 204)
     }))
   }
-}
-
-async function handleAllTSDs(options) {
-  const cql = 'space=AT and type=page and (label="se-opportunity" or label="se-tsd")'
-  let url = encodeURI(`https://${options.domain}.atlassian.net/wiki/rest/api/content/search?cql=${cql}&limit=50`)
-  do {
-    const responseJson = await cqlSearch(options, url)
-    responseJson.results.forEach(page => console.info(`${page.id}: ${page.title}`))
-    console.info(`start: ${responseJson.start}, limit: ${responseJson.limit}, size: ${responseJson.size}`)
-    if (responseJson._links.next) {
-      url = responseJson._links.base + responseJson._links.next
-    } else {
-      url = undefined
-    }
-  } while (url)
-}
-
-async function cqlSearch(options, url) {
-  console.debug(url)
-  const response = await fetch(url, {
-    method: 'GET',
-    headers: {
-      'Authorization': `Basic ${Buffer.from(`${options.forgeEmail}:${options.forgeApiToken}`).toString('base64')}`,
-      'Accept': 'application/json'
-    }
-  });
-  if (response.status !== 200) {
-    console.error(`Response: ${response.status} ${response.statusText}`);
-    console.error(await response.text());
-    return null;
-  }
-  return await response.json()
 }
